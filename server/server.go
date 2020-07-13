a04fe6c90ae527a41bde5cdabcfb1a4c6d10dfa4
asawo <asawo@mercari.com>
2020-07-13 23:03:14 +0900

    added connection to local psql test db

--- server/server.go
+++ server/server.go
@@ -1,10 +1,21 @@
 package main
 
 import (
+	"database/sql"
 	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
+
+	_ "github.com/lib/pq"
+)
+
+const (
+	host     = "localhost"
+	port     = 5432
+	user     = "postgres"
+	password = ""
+	dbname   = "test"
 )
 
 // User struct contains user data
@@ -17,8 +28,6 @@ type User struct {
 var Users []User
 
 func home(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("request", r.Method)
-	fmt.Println("writer", w)
 	fmt.Fprintf(w, "Welcome to Home!")
 	fmt.Println("Req made to endpoint: home")
 }
@@ -34,7 +43,38 @@ func handleRequests() {
 	http.HandleFunc("/users", getUsers)
 }
 
+func connectToDb() {
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Successfully connected!")
+
+	var myUser User
+	query := `SELECT * FROM test WHERE id = $1`
+	err = db.QueryRow(query, 1).Scan(&myUser.ID, &myUser.Name)
+	if err != nil {
+		log.Fatal("Failed to execute query: ", err)
+	}
+
+	fmt.Printf("Hi %s, welcome back!\n", myUser.Name)
+}
+
 func main() {
+
+	connectToDb()
+
 	handleRequests()
 
 	Users = []User{
