c416d7f64e1da122c7c7f63766a2b7ed844e9c8b
asawo <asawo@mercari.com>
2020-07-13 22:00:02 +0900

    added users endpoint

--- server/server.go
+++ server/server.go
@@ -1,17 +1,47 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 )
 
+// User struct contains user data
+type User struct {
+	ID   int    `json:"userid"`
+	Name string `json:"username"`
+}
+
+// Users contains multiple user data
+var Users []User
+
 func home(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("Home!")
+	fmt.Println("request", r.Method)
+	fmt.Println("writer", w)
+	fmt.Fprintf(w, "Welcome to Home!")
+	fmt.Println("Req made to endpoint: home")
+}
+
+func getUsers(w http.ResponseWriter, r *http.Request) {
+
+	json.NewEncoder(w).Encode(Users)
+	fmt.Println("Req made to endpoint: users")
+}
+
+func handleRequests() {
+	http.HandleFunc("/", home)
+	http.HandleFunc("/users", getUsers)
 }
 
 func main() {
-	http.HandleFunc("/", home)
+	handleRequests()
+
+	Users = []User{
+		User{ID: 1, Name: "Arthur"},
+		User{ID: 2, Name: "Testmothy"},
+	}
+
 	fmt.Println("Server is running on port :8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
